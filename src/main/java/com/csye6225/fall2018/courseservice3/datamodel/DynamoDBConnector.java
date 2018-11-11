27c87997a1108d54baa65c8207833769bc5d0d8f
Bharti <ladhani.b@husky.neu.edu>
2018-11-11 13:46:14 -0800

    Pushing connector

---
+++ src/main/java/com/csye6225/fall2018/courseservice3/datamodel/DynamoDBConnector.java
@@ -0,0 +1,40 @@
+package com.csye6225.fall2018.courseservice3.datamodel;
+
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
+public class DynamoDBConnector {
+
+	static AmazonDynamoDB dynamoDB;
+
+	public static void init() {
+		try {
+			if (dynamoDB == null) {
+				InstanceProfileCredentialsProvider credentialsProvider = new InstanceProfileCredentialsProvider(false);
+				credentialsProvider.getCredentials();
+
+				dynamoDB = AmazonDynamoDBClientBuilder.standard().withCredentials(credentialsProvider)
+						.withRegion("us-east-2").build();
+				System.out.println("I created the client");
+			}
+		} catch (Exception e) {
+			if (dynamoDB == null) {
+				ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+				credentialsProvider.getCredentials();
+
+				dynamoDB = AmazonDynamoDBClientBuilder.standard().withCredentials(credentialsProvider)
+						.withRegion("us-east-2").build();
+				System.out.println("I created the client");
+
+			}
+		}
+
+	}
+
+	public AmazonDynamoDB getClient() {
+		return dynamoDB;
+	}
+
+}
