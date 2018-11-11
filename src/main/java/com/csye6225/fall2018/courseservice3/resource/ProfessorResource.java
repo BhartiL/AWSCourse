7d24b70930be592006c20ab78c0f1d35fcf13efe
Bharti <ladhani.b@husky.neu.edu>
2018-11-11 13:40:51 -0800

    Pushing Professor Resource

--- src/main/java/com/csye6225/fall2018/courseservice3/resource/ProfessorResource.java
+++ src/main/java/com/csye6225/fall2018/courseservice3/resource/ProfessorResource.java
@@ -1,8 +1,5 @@
 package com.csye6225.fall2018.courseservice3.resource;
 
-import java.util.Date;
-import java.util.List;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,7 +8,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.csye6225.fall2018.courseservice3.datamodel.Professor;
@@ -23,34 +19,17 @@ public class ProfessorResource {
 
 	ProfessorService profService = new ProfessorService();
 
-	/*@GET
-
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Professor> getProfessors() {
-		return profService.getAllProfessors();
-	}*/
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Professor> getProfessorsByDeparment(@QueryParam("department") String department) {
-		if (department == null) {
-			return profService.getAllProfessors();
-		}
-		return profService.getProfessorsByDepartment(department);
-	}
-
-	// ... webapi/professor/1
 	@GET
 	@Path("/{professorId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Professor getProfessor(@PathParam("professorId") long profId) {
+	public Professor getProfessor(@PathParam("professorId") String profId) {
 		return profService.getProfessor(profId);
 	}
 
 	@DELETE
 	@Path("/{professorId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Professor deleteProfessor(@PathParam("professorId") long profId) {
+	public Professor deleteProfessor(@PathParam("professorId") String profId) {
 		return profService.deleteProfessor(profId);
 	}
 
@@ -65,11 +44,8 @@ public class ProfessorResource {
 	@Path("/{professorId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Professor updateProfessor(@PathParam("professorId") long profId, Professor prof) {
+	public Professor updateProfessor(@PathParam("professorId") String profId, Professor prof) {
 		return profService.updateProfessorInformation(profId, prof);
 	}
 
-	public void addProfessor(String name, String department, Date joiningDate) {
-		profService.addProfessor(name, department, joiningDate);
-	}
 }
