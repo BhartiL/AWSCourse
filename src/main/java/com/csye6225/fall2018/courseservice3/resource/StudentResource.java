64a4c936de545cab6b78b5c986f75a9d8a865644
Bharti <ladhani.b@husky.neu.edu>
2018-11-11 13:40:30 -0800

    Pushing Student Resource

--- src/main/java/com/csye6225/fall2018/courseservice3/resource/StudentResource.java
+++ src/main/java/com/csye6225/fall2018/courseservice3/resource/StudentResource.java
@@ -1,6 +1,5 @@
 package com.csye6225.fall2018.courseservice3.resource;
 
-import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -9,7 +8,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.csye6225.fall2018.courseservice3.datamodel.Student;
@@ -20,34 +18,16 @@ public class StudentResource {
 	StudentService stuService = new StudentService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Student> getStudents() {
-		return stuService.getAllStudents();
-	}
-
-	@GET
-	@Path("/program/{programId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Student> getStudentByProgramId(@QueryParam("programId") String programId) {
-		if (programId == null) {
-			return stuService.getAllStudents();
-		}
-		return stuService.getStudentsByProgram(programId);
-
-	}
-
-	// ... webapi/student/1
-	@GET
 	@Path("/{studentId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Student getStudent(@PathParam("studentId") long studentId) {
+	public Student getStudent(@PathParam("studentId") String studentId) {
 		return stuService.getStudent(studentId);
 	}
 
 	@DELETE
 	@Path("/{studentId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Student deleteStudent(@PathParam("studentId") long studentId) {
+	public Student deleteStudent(@PathParam("studentId") String studentId) {
 		return stuService.deleteStudent(studentId);
 	}
 
@@ -62,7 +42,7 @@ public class StudentResource {
 	@Path("/{studentId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Student updateStudent(@PathParam("studentId") long studentId, Student stu) {
+	public Student updateStudent(@PathParam("studentId") String studentId, Student stu) {
 		return stuService.updateStudentInformation(studentId, stu);
 	}
 
@@ -70,18 +50,9 @@ public class StudentResource {
 	@Path("/{studentId}/course/{courseId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Student enrollstudent(@PathParam("courseId") String courseId, @PathParam("studentId") long studentId) {
+	public Student enrollStudentToCourse(@PathParam("courseId") String courseId, @PathParam("studentId") String studentId) {
 		return stuService.enrollStudentToCourse(courseId, studentId);
 
-	}
-
-	@PUT
-	@Path("/{studentId}/program/{programId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Student enrollStudentToProgram(@PathParam("programId") String programId,
-			@PathParam("studentId") long studentId) {
-		return stuService.enrollStudentInProgram(programId, studentId);
 	}
 
 }
