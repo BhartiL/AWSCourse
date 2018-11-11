24b39d52cd009f9fe1dc3bcf20ca820e7461d077
Bharti <ladhani.b@husky.neu.edu>
2018-11-11 13:39:04 -0800

    Pushing Program Service

--- src/main/java/com/csye6225/fall2018/courseservice3/service/ProfessorService.java
+++ src/main/java/com/csye6225/fall2018/courseservice3/service/ProfessorService.java
@@ -1,76 +1,66 @@
 package com.csye6225.fall2018.courseservice3.service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-
-import com.csye6225.fall2018.courseservice3.datamodel.InMemoryDatabase;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.csye6225.fall2018.courseservice3.datamodel.DynamoDBConnector;
 import com.csye6225.fall2018.courseservice3.datamodel.Professor;
 
 public class ProfessorService {
 
-	static HashMap<Long, Professor> prof_Map = InMemoryDatabase.getProfessorDB();
+	DynamoDBConnector dynamoDB;
+	DynamoDBMapper mapper;
 
-	// Getting a list of all professor
-	// GET "..webapi/professors"
-	public List<Professor> getAllProfessors() {
-		// Getting the list
-		ArrayList<Professor> list = new ArrayList<>();
-		for (Professor prof : prof_Map.values()) {
-			list.add(prof);
-		}
-		return list;
+	public ProfessorService() {
+		dynamoDB = new DynamoDBConnector();
+		DynamoDBConnector.init();
+		mapper = new DynamoDBMapper(dynamoDB.getClient());
 	}
 
-	// Adding professor through parameterized constructor
-	public void addProfessor(String name, String department, Date joiningDate) {
-		// Next Id
-		long nextAvailableId = prof_Map.size() + 1;
-		// Create a Professor Object
-		Professor prof = new Professor(nextAvailableId, name, department, joiningDate);
-		prof_Map.put(nextAvailableId, prof);
-	}
-
-	// adding professor 
+	// adding professor
 	public Professor addProfessor(Professor prof) {
-		long nextAvailableId = prof_Map.size() + 1;
-		prof.setProfessorId(nextAvailableId);
-		prof_Map.put(nextAvailableId, prof);
-		return prof_Map.get(nextAvailableId);
-	}
-
-	// Getting One Professor
-	public Professor getProfessor(Long profId) {
-		return prof_Map.get(profId);
-	}
-
-	// Deleting a professor
-	public Professor deleteProfessor(Long profId) {
-		Professor deletedProfDetails = prof_Map.get(profId);
-		prof_Map.remove(profId);
-		return deletedProfDetails;
-	}
-
-	// Updating Professor Info
-	public Professor updateProfessorInformation(Long profId, Professor prof) {
-		Professor oldProfObject = prof_Map.get(profId);
-		profId = oldProfObject.getProfessorId();
-		prof.setProfessorId(profId);
-		// Publishing New Values
-		prof_Map.put(profId, prof);
+		mapper.save(prof);
 		return prof;
 	}
 
-	// Get professors in a department
-	public List<Professor> getProfessorsByDepartment(String department) {
-		// Getting the list
-		ArrayList<Professor> list = new ArrayList<>();
-		for (Professor prof : prof_Map.values()) {
-			if (prof.getDepartment().equals(department)) {
-				list.add(prof);
-			}
+	// Getting One Professor
+	public Professor getProfessor(String profId) {
+		try {
+			Professor partitionKey = new Professor();
+			partitionKey.setProfessorId(profId);
+			DynamoDBQueryExpression<Professor> queryExpression = new DynamoDBQueryExpression<>();
+			queryExpression.setHashKeyValues(partitionKey);
+			queryExpression.setIndexName("professorId-index");
+			queryExpression.setConsistentRead(false);
+			List<Professor> prof = mapper.query(Professor.class, queryExpression);
+			if (prof == null || prof.isEmpty())
+				throw new Exception("No professor found with professor id " + profId);
+			return prof.get(0);
+		} catch (Exception e) {
+			e.getMessage();
+			e.printStackTrace();
+			return null;
 		}
-		return list;
 	}
+
+	// Deleting a professor
+	public Professor deleteProfessor(String profId) {
+		Professor prof = getProfessor(profId);
+		String id = prof.getId();
+		Professor objTodelete = mapper.load(Professor.class, id);
+		objTodelete.setId(id);
+		mapper.delete(objTodelete);
+		System.out.println("This professor is deleted");
+		return prof;
+	}
+
+	// Updating Professor Info
+	public Professor updateProfessorInformation(String profId, Professor prof) {
+		Professor profdetails = this.getProfessor(profId);
+		prof.setId(profdetails.getId());
+		prof.setProfessorId(profId);
+		mapper.save(prof);
+		return prof;
+	}
+
 }
